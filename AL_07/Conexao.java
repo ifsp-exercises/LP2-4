37611b8352df68a975595c072db6ca454108fa34
Angelo Evangelista <angeloevan.ane@gmail.com>
2020-12-09 19:06:31 -0300

    Aula 07

---
+++ AL_07/Conexao.java
@@ -0,0 +1,29 @@
+import java.sql.*;
+
+public class Conexao {
+  public static void main(String args[]) {
+    try {
+      String url = "jdbc:sqlserver://127.0.0.1\\SQLEXPRESS;databaseName=AulaJava;integratedSecurity=true";
+
+      Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+      Connection con;
+      con = DriverManager.getConnection(url);
+
+      System.out.println("Conexao OK");
+
+      // dados da conexao
+      DatabaseMetaData db = con.getMetaData();
+      System.out.println("Conectado a " + db.getURL());
+      System.out.println("Conexao " + db.getConnection());
+      System.out.println("Driver " + db.getDriverName());
+      System.out.println("Usuario " + db.getUserName());
+      
+      System.out.println("----------------------------------");
+
+      con.close();
+    } catch (Exception e) {
+      System.out.println("Erro: " + e.getMessage());
+    }
+  }
+}
